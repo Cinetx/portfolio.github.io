79df9d3642f865b4cafaa789304280e5ba6c35a0
Cinetx <39036064+Cinetx@users.noreply.github.com>
2019-11-25 23:38:44 +0300

    Add files via upload

--- js/navigation.js
+++ js/navigation.js
@@ -1,13 +1,3 @@
-// Логика следующая при клике на бургер открывается меню или закрывваетсчя
-// При скролле вниз меню пропадает
-// При скролле вверх меню появляется
-
-// var path = document.querySelector('.works-title path');
-// console.log(path.getTotalLength());
-
-// var path = document.querySelector('.skills-title path');
-// for (let i = 0; i < path; i++){
-// console.log(path.getTotalLength());}
 
 var aboutMe = document.querySelector(".nav-list--about-me");
 var mySkills = document.querySelector(".nav-list--my-skills");
@@ -27,73 +17,27 @@ window.onscroll = function() {
 // Добавление анимации
     if (scrollTop > 100) {
       skills_title.classList.add('skills_animation');
+      aboutMe.classList.remove("nav-list--active");
+      mySkills.classList.add("nav-list--active");
+      myWork.classList.remove("nav-list--active");
 
       if (scrollTop > 700) {
         works_title.classList.add('skills_animation');
-
-  if (scrollTop > 200) {
-    aboutMe.classList.remove("nav-list--active");
-    mySkills.classList.add("nav-list--active");
-    myWork.classList.remove("nav-list--active");
 
     if (scrollTop > 1300) {
       aboutMe.classList.remove("nav-list--active");
       mySkills.classList.remove("nav-list--active");
       myWork.classList.add("nav-list--active");
     }
-  }}}}};
-//
-// aboutMe.addEventListener("click", function () {
-//   window.scroll({
-//     top: 1,
-//     left: 0,
-//     behavior: 'smooth'
-//   });
-// });
-//
-// mySkills.addEventListener("click", function () {
-//   window.scroll({
-//     top: 700,
-//     left: 0,
-//     behavior: 'smooth'
-//   });
-//   // 0, 700);
-//
-// });
-//
-// myWork.addEventListener("click", function () {
-//   window.scroll({
-//     top: 1900,
-//     left: 0,
-//     behavior: 'smooth'
-//   });
-// });
-//
-// mySkills.addEventListener("click", function () {
-//   window.classList.add(".my-skills__animation");
-// });
+  }}}};
 
+
+
+// плавное перемещение страницы к нужному блоку
 $(document).ready(function(){
-  // плавное перемещение страницы к нужному блоку
   $("nav a").click(function () {
     elementClick = $(this).attr("href");
     destination = $(elementClick).offset().top;
     $("body,html").animate({scrollTop: destination }, 800);
   });
 });
-
-// const logo = document.querySelectorAll("#logo path");
-//
-// for (let i = 0; i < logo.length; i++){
-//   console.log('Letter ${i} is ${logo[i].getTotalLength()}');
-// }
-// console.log(logo);
-
-
-
-//
-// $(window).scroll(function(){
-//   if ( $(this).scrollTop() > el.offset().top - 200 ) {
-//     skills_animation.addClass('skills_animation');
-//   }
-// });
