1fc5a308ee043f251da78561240f52411c152392
Cinetx <39036064+Cinetx@users.noreply.github.com>
2020-06-16 23:29:14 +0400

    Add files via upload

--- js/logo.js
+++ js/logo.js
@@ -1,91 +1,20 @@
-var linkHtmlLogo = document.querySelector(".HTML-logo__link");
-var svgHtmlLogo = document.querySelector(".html-logo");
+'use strict'
 
-linkHtmlLogo.addEventListener("mouseover" ,function(){
-  svgHtmlLogo.classList.add("logo-animation");
-});
+let linkLogo = document.querySelectorAll('.my-skills__link'),
+    imgLogo = document.querySelectorAll('.my-skills__logo');
 
-linkHtmlLogo.addEventListener("mouseout" ,function(){
-  svgHtmlLogo.classList.remove("logo-animation");
-});
-
-var linkHtmAcademylLogo = document.querySelector(".htmlacademy-logo__link");
-var svgHtmAcademylLogo = document.querySelector(".htmlacademy-logo");
-
-linkHtmAcademylLogo.addEventListener("mouseover" ,function(){
-  svgHtmAcademylLogo.classList.add("logo-animation");
-});
-
-linkHtmAcademylLogo.addEventListener("mouseout" ,function(){
-  svgHtmAcademylLogo.classList.remove("logo-animation");
-});
-
-
-var linkGulpLogo = document.querySelector(".gulp-logo__link");
-var svgGulpLogo = document.querySelector(".gulp-logo");
-
-linkGulpLogo.addEventListener("mouseover" ,function(){
-  svgGulpLogo.classList.add("logo-animation");
-});
-
-linkGulpLogo.addEventListener("mouseout" ,function(){
-  svgGulpLogo.classList.remove("logo-animation");
-});
-
-var linkSassLogo = document.querySelector(".sass-logo__link");
-var svgSassLogo = document.querySelector(".sass-logo");
-
-linkSassLogo.addEventListener("mouseover" ,function(){
-  svgSassLogo.classList.add("logo-animation");
-});
-
-linkSassLogo.addEventListener("mouseout" ,function(){
-  svgSassLogo.classList.remove("logo-animation");
-});
-
-var linkLessLogo = document.querySelector(".less-logo__link");
-var svgLessLogo = document.querySelector(".less-logo");
-
-linkLessLogo.addEventListener("mouseover" ,function(){
-  svgLessLogo.classList.add("logo-animation");
-});
-
-linkLessLogo.addEventListener("mouseout" ,function(){
-  svgLessLogo.classList.remove("logo-animation");
-});
-
-// var linkCssLogo = document.querySelector(".css-logo__link");
-// var svgCssLogo = document.querySelector(".css-logo");
-
-// linkCssLogo.addEventListener("mouseover" ,function(){
-//   svgCssLogo.classList.add("logo-animation");
-// });
-
-// linkCssLogo.addEventListener("mouseout" ,function(){
-//   svgCssLogo.classList.remove("logo-animation");
-// });
-
-var linkBEMLogo = document.querySelector(".BEM-logo__link");
-var svgBEMLogo = document.querySelector(".BEM-logo");
-
-linkBEMLogo.addEventListener("mouseover" ,function(){
-  svgBEMLogo.classList.add("logo-animation");
-});
-
-linkBEMLogo.addEventListener("mouseout" ,function(){
-  svgBEMLogo.classList.remove("logo-animation");
-});
-
-
-var linkJSLogo = document.querySelector(".JS-logo__link");
-var svgJSLogo = document.querySelector(".JS-logo");
-
-linkJSLogo.addEventListener("mouseover" ,function(){
-  svgJSLogo.classList.add("logo-animation");
-});
-
-linkJSLogo.addEventListener("mouseout" ,function(){
-  svgJSLogo.classList.remove("logo-animation");
-});
+linkLogo.forEach(linkLogo =>{
+    linkLogo.addEventListener('mouseover', function(){
+      var currnetLogo = this.getAttribute("data-type");
+      imgLogo.forEach(imgLogo => {
+          if (imgLogo.getAttribute('data-type') == currnetLogo == true) {
+              imgLogo.classList.add('logo-animation');
+          }
+          linkLogo.addEventListener('mouseout', function(){
+            imgLogo.classList.remove('logo-animation');
+          })
+        });
+    });
+  });
 
 
